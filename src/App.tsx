e620253ead635e331330c49b52c08457f905a3b2
Wither19 <josephvranicar1@gmail.com>
2025-03-09 18:59:49 -0400

    c

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,20 @@ import * as bootstrap from "bootstrap";
 function App() {
 	const [draftText, setDraftText] = useState("");
 
+	const [todos, setTodos] = useState([
+		{
+			done: true,
+			name: "Sample task",
+		},
+	]);
+
+	const updateDraft = () => {
+		const draft = document.getElementById("todo-draft") as HTMLInputElement;
+		setDraftText(draft.value);
+	};
+
+	const submitDraft = () => {};
+
 	return (
 		<>
 			<div className="container-md">
@@ -16,7 +30,23 @@ function App() {
 					</div>
 					<div className="col-4"></div>
 				</div>
-				<div className="row"></div>
+				<br />
+				<br />
+				<div className="row">
+					<div className="col-4"></div>
+					<div className="col-8">
+						<input
+							type="text"
+							id="todo-draft"
+							value={draftText}
+							onChange={updateDraft}
+							onKeyUp={submitDraft}
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-8"></div>
+				</div>
 			</div>
 		</>
 	);
