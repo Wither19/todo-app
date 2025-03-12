b3ee6622aef856f6e7493366ccfce0f686ac4185
wither19 <146484561+Wither19@users.noreply.github.com>
2025-03-12 17:54:12 +0000

    c

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,20 @@ import "./scss/App.scss";
 import TodoInternal from "./TodoInternal";
 
 function App() {
-	return (
-		<>
-			<div className="container-md">
-				<div className="row">
-					<div className="col-4"></div>
-					<div className="col-4">
-						<div className="display-3">Todo App</div>
-					</div>
-					<div className="col-4"></div>
-				</div>
-				<br />
-				<br />
-				<TodoInternal />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container-md">
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-4">
+            <div className="display-3">Todo App</div>
+          </div>
+          <div className="col-4"></div>
+        </div>
+        <TodoInternal />
+      </div>
+    </>
+  );
 }
 
 export default App;
